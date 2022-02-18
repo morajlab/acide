6cab8b117bedba620af56f48b030255e425fea53
Morteza Jamali <mortezajamali4241@gmail.com>
2022-02-18 21:14:14 +0000

    fix(dependencies): change package `monaco-editor` version

--- packages/ui/ui/src/app/app.tsx
+++ packages/ui/ui/src/app/app.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 //import styles from './app.module.css';
-import { MoleculeProvider, Workbench } from '@dtinsight/molecule';
+import React from 'react';
+import { MoleculeProvider, Workbench } from '.';
 import '@dtinsight/molecule/esm/style/mo.css';
 
 export const App = () => (
